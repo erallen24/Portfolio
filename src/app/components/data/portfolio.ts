4a036073b8c065c7aa770305b887ade407c6e482
Evan Allen <contact@evanallen.dev>
2026-06-01 22:02:41 -0400

    path fix

--- src/app/components/data/portfolio.ts
+++ src/app/components/data/portfolio.ts
@@ -41,7 +41,7 @@ export const profileData = {
   roles: ["Game Developer", "Software Engineer", "System Architect", "Tech Innovator"],
   experience: "5+ Years Experience",
   availability: "Available for hire",
-  resume: "EvanAllen_Resume.pdf", 
+  resume: "/public/EvanAllen_Resume.pdf", 
   socials: {
     github: "https://github.com/erallen24",
     linkedin: "https://www.linkedin.com/in/evan-allen-game-dev/",
